0b9c12d994f759c96646c081f46696ba77ace879
Aarti Bisht <aartbisht@gmail.com>
2025-07-01 13:48:41 +0530

    Update NewCollections.jsx

--- src/NewCollections/NewCollections.jsx
+++ src/NewCollections/NewCollections.jsx
@@ -9,7 +9,7 @@ const NewCollections = () =>{
 
 
     useEffect(()=>{
-fetch('http://localhost:4000/newcollections')
+fetch('https://e-commerce-backend-1-ix83.onrender.com/newcollections')
 .then((response)=>response.json())
 .then((data)=>setNew_collection(data));
     },[])
