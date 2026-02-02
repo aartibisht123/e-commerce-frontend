86e86c25a817792c50d74c82765dd15a0d20364f
Aarti Bisht <aartbisht@gmail.com>
2026-02-02 13:20:04 +0530

    Change data_product to popularProducts in Popular.jsx

--- src/Popular/Popular.jsx
+++ src/Popular/Popular.jsx
@@ -19,7 +19,7 @@ fetch('https://e-commerce-backend-1-ix83.onrender.com/popularinwomen').then((res
 <h1>POPULAR IN WOMEN</h1>
 <hr />
 <div className="popular-item">
-{data_product.map((item,i) => {
+{popularProducts.map((item,i) => {
 return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
 })}
   
