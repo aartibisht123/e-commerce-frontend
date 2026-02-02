15a06a1fd81187095b0ed5a079e94be45e1602f1
Aarti Bisht <aartbisht@gmail.com>
2026-02-02 13:06:03 +0530

    Return null if product is not provided
    Add null check for product in BreadCrum component

--- src/BreadCrums/BreadCrum.jsx
+++ src/BreadCrums/BreadCrum.jsx
@@ -3,6 +3,7 @@ import './BreadCrum.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png'
 const BreadCrum = (props)=> {
     const {product} = props;
+      if (!product) return null; 
 return(
     <div className="breadcrum">
 HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
