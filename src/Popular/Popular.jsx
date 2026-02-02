e967dc738f03f961ab817c63aafcc077e8d2503f
Aarti Bisht <aartbisht@gmail.com>
2026-02-02 15:21:46 +0530

    Refactor Popular component to use products state

--- src/Popular/Popular.jsx
+++ src/Popular/Popular.jsx
@@ -5,21 +5,25 @@ import Item from '../Item/Item';
 
 const Popular = () => {
 
-    const [popularProducts,setPopularProducts] = useState([]);
+   const [products, setProducts] = useState([]);
 
-useEffect(()=>{
-fetch('https://e-commerce-backend-1-ix83.onrender.com/popularinwomen').then((response)=>response.json())
-.then((data)=>setPopularProducts(data));
-},[])
-    return(
-        <div className="popular">
-<h1>POPULAR IN WOMEN</h1>
-<hr />
+useEffect(() => {
+  fetch('https://e-commerce-backend-1-ix83.onrender.com/popularinwomen')
+    .then(res => res.json())
+    .then(data => setProducts(data));
+}, []);
+
 <div className="popular-item">
-{popularProducts.map((item,i) => {
-return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-})}
-  
+  {products.map((item, i) => (
+    <Item
+      key={i}
+      id={item.id}
+      name={item.name}
+      image={item.image}
+      new_price={item.new_price}
+      old_price={item.old_price}
+    />
+  ))}
 </div>
         </div>
     )
