dff1381444b58b0467862edf84123fc589ee2e57
Aarti Bisht <aartbisht@gmail.com>
2026-02-06 19:50:58 +0530

    Add files via upload

--- src/Popular/Popular.jsx
+++ src/Popular/Popular.jsx
@@ -1,32 +1,29 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Popular.css'
-import Item from '../Item/Item'
+// import data_product from '../Assets/data'
+import Item from '../Item/Item';
 
 const Popular = () => {
-  const [products, setProducts] = useState([])
 
-  useEffect(() => {
-    fetch('https://e-commerce-backend-1-ix83.onrender.com/popularinwomen')
-      .then(res => res.json())
-      .then(data => setProducts(data))
-  }, [])
+    const [popularProducts,setPopularProducts] = useState([]);
 
-  return (
-    <div className="popular">
-      <div className="popular-item">
-        {products.map((item, i) => (
-          <Item
-            key={i}
-            id={item.id}
-            name={item.name}
-            image={item.image}
-            new_price={item.new_price}
-            old_price={item.old_price}
-          />
-        ))}
-      </div>
-    </div>
-  )
+useEffect(()=>{
+fetch('http://localhost:4000/popularinwomen').then((response)=>response.json())
+.then((data)=>setPopularProducts(data));
+},[])
+    return(
+        <div className="popular">
+<h1>POPULAR IN WOMEN</h1>
+<hr />
+<div className="popular-item">
+{popularProducts.map((item,i) => {
+return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+})}
+  
+</div>
+        </div>
+    )
 }
 
 export default Popular
+
