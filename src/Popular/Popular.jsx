56c35ca5a8e4ebf82195e30acc588d6bb6dce7d5
Aarti Bisht <aartbisht@gmail.com>
2026-02-06 19:09:57 +0530

    Refactor Popular component to clean up code

--- src/Popular/Popular.jsx
+++ src/Popular/Popular.jsx
@@ -1,37 +1,32 @@
-/* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './Popular.css'
-// import data_product from '../Assets/data'
-import Item from '../Item/Item';
+import Item from '../Item/Item'
 
 const Popular = () => {
+  const [products, setProducts] = useState([])
 
-   const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch('https://e-commerce-backend-1-ix83.onrender.com/popularinwomen')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+  }, [])
 
-useEffect(() => {
-  fetch('https://e-commerce-backend-1-ix83.onrender.com/popularinwomen')
-    .then(res => res.json())
-    .then(data => setProducts(data));
-}, []);
-
-<div className="popular-item">
-  {products.map((item, i) => (
-    <Item
-      key={i}
-      id={item.id}
-      name={item.name}
-      image={item.image}
-      new_price={item.new_price}
-      old_price={item.old_price}
-    />
-  ))}
-</div>
-        </div>
-    )
+  return (
+    <div className="popular">
+      <div className="popular-item">
+        {products.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Popular
-
-
-
-
